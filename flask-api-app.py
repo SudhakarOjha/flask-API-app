0a6f37779f879d36602fbb9f9055ab32fd43e2b2
Sudhakar Ojha <146667332+SudhakarOjha@users.noreply.github.com>
2025-07-13 22:00:35 +0530

    flask-api-app.py

--- flask-api-app.py
+++ flask-api-app.py
@@ -2,14 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/LW")
 def LW(): 
+    return "This is returns LW"
     print("this is LW")
 @app.route("/college")
 def git(): 
-    return "hello"
+    return "This returns git"
     print("GLobal Institute of technology")
 
 @app.route('/Home')
 def Agra(): 
     print("TAJ MAHAL")
+    return "Agra is part of Uttar Pradesh"
 
 app.run()
